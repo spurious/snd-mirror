bb32f97ec249334e5cd910eb0628a64a81649138
schottstaedt <schottstaedt>
2000-09-27 12:35:06 +0000

    effects menu

--- snd-x1.h
+++ snd-x1.h
@@ -170,7 +170,7 @@ void add_popup_handler(Widget w);
 
 int gh_change_menu_label(int which_menu,char *old_label, char *new_label);
 int gh_set_menu_sensitive(int which_menu,char *old_label, int on);
-int gh_add_to_main_menu(snd_state *ss, char *label);
+int gh_add_to_main_menu(snd_state *ss, char *label, int slot);
 int gh_add_to_menu(snd_state *ss, int which_menu, char *label, int callb);
 int gh_remove_from_menu(int which_menu, char *label);
 int gh_menu_is_sensitive(int which_menu,char *old_label);
