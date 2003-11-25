f744d4a9292566f834545b614f2a7a92d506d843
schottstaedt <schottstaedt>
2003-11-25 11:37:25 +0000

    bugs

---
+++ snd7.scm
@@ -0,0 +1,5 @@
+;;; backwards compatibility for snd 7
+
+(define free-mix-sample-reader free-sample-reader)
+(define free-track-sample-reader free-sample-reader)
+(define inspect-sample-reader describe-sample-reader)
