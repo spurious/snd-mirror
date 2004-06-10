b3e1b44cee84ee3c814ccf9cae1fc75a689c950d
schottstaedt <schottstaedt>
2004-06-10 11:14:53 +0000

    bugs

--- dlp/fft-menu.scm
+++ dlp/fft-menu.scm
@@ -1,8 +1,9 @@
-(use-modules (ice-9 format))
+(use-modules (ice-9 format) (ice-9 optargs))
 
 (provide 'snd-fft-menu.scm)
 
 ;;; TODO: add gtk support here and in other dlp additions
+;;; DONE: gtk support (KSM)
 
 (define fft-list '()) ; menu labels are updated to show current default settings
 
@@ -30,32 +31,39 @@
     (begin
 
       (define (post-fft-edit-dialog)
-        (if (not (Widget? fft-edit-dialog))
+        (if (not fft-edit-dialog)
             ;; if fft-edit-dialog doesn't exist, create it
             (let ((initial-fft-edit-low-frequency 100)
                   (initial-fft-edit-high-frequency 1000)
                   (sliders '()))
               (set! fft-edit-dialog
                     (make-effect-dialog fft-edit-label
-                                        (lambda (w context info)
+                                        (lambda* (w context #:optional info)
                                           (cp-fft-edit))
-                                        (lambda (w context info)
+                                        (lambda* (w context #:optional info)
                                           (help-dialog "FFT notch filter"
                                                        "A simple example of FFT-based editing. It takes an FFT of the entire sound, removes all energy below the low frequency and above the high frequency, then computes the inverse FFT."))
-                                        (lambda (w c i)
-                                          (set! fft-edit-low-frequency initial-fft-edit-low-frequency)
-                                          (XtSetValues (list-ref sliders 0) (list XmNvalue (inexact->exact (* fft-edit-low-frequency 1))))
-                                          (set! fft-edit-high-frequency initial-fft-edit-high-frequency)
-                                          (XtSetValues (list-ref sliders 1) (list XmNvalue (inexact->exact (* fft-edit-high-frequency 1)))))))
+                                        (lambda* (w c #:optional i)
+						 (set! fft-edit-low-frequency initial-fft-edit-low-frequency)
+						 (set! fft-edit-high-frequency initial-fft-edit-high-frequency)
+						 (if (provided? 'snd-gtk)
+						     (begin
+						       (set! (.value (GTK_ADJUSTMENT (car sliders)))  (inexact->exact (* fft-edit-low-frequency 1)))
+						       (gtk_adjustment_value_changed (GTK_ADJUSTMENT (car sliders)))
+						       (set! (.value (GTK_ADJUSTMENT (cadr sliders)))  (inexact->exact (* fft-edit-high-frequency 1)))
+						       (gtk_adjustment_value_changed (GTK_ADJUSTMENT (cadr sliders))))
+						     (begin
+						       (XtSetValues (list-ref sliders 0) (list XmNvalue (inexact->exact (* fft-edit-low-frequency 1))))
+						       (XtSetValues (list-ref sliders 1) (list XmNvalue (inexact->exact (* fft-edit-high-frequency 1)))))))))
               (set! sliders
                    (add-sliders fft-edit-dialog
                                  (list (list "low frequency" 20 initial-fft-edit-low-frequency 22050
-                                             (lambda (w context info)
-                                               (set! fft-edit-low-frequency (/ (.value info) 1)))
+                                             (lambda* (w context #:optional info)
+                                               (set! fft-edit-low-frequency (/ (.value (if (provided? 'snd-gtk) (GTK_ADJUSTMENT w) info)) 1)))
                                              1)
                                        (list "high frequency" 20 initial-fft-edit-high-frequency 22050
-                                             (lambda (w context info)
-                                               (set! fft-edit-high-frequency (/ (.value info) 1)))
+                                             (lambda* (w context #:optional info)
+                                               (set! fft-edit-high-frequency (/ (.value (if (provided? 'snd-gtk) (GTK_ADJUSTMENT w) info)) 1)))
                                              1))))))
         (activate-dialog fft-edit-dialog))
 
@@ -86,25 +94,32 @@
     (begin
 
       (define (post-fft-squelch-dialog)
-        (if (not (Widget? fft-squelch-dialog))
+        (if (not fft-squelch-dialog)
             ;; if fft-squelch-dialog doesn't exist, create it
             (let ((initial-fft-squelch-amount 0.0)
                   (sliders '()))
               (set! fft-squelch-dialog
                     (make-effect-dialog fft-squelch-label
-                                        (lambda (w context info)
+                                        (lambda* (w context #:optional info)
                                           (cp-fft-squelch))
-                                        (lambda (w context info)
+                                        (lambda* (w context #:optional info)
                                           (help-dialog "FFT squelch"
                                                 "Removes all energy below the squelch amount. This is sometimes useful for noise-reduction."))
-                                        (lambda (w c i)
+                                        (lambda* (w c #:optional i)
                                           (set! fft-squelch-amount initial-fft-squelch-amount)
-                                          (XtSetValues (list-ref sliders 0) (list XmNvalue (inexact->exact (round (* fft-squelch-amount 100))))))))
+					  (if (provided? 'snd-gtk)
+					      (begin
+						(set! (.value (GTK_ADJUSTMENT (car sliders)))  (inexact->exact (round (* fft-squelch-amount 100))))
+						(gtk_adjustment_value_changed (GTK_ADJUSTMENT (car sliders))))
+					      (XtSetValues (list-ref sliders 0) (list XmNvalue (inexact->exact (round (* fft-squelch-amount 100)))))))))
               (set! sliders
                     (add-sliders fft-squelch-dialog
                                  (list (list "squelch amount" 0.0 initial-fft-squelch-amount 1.0
-                                             (lambda (w context info)
-                                               (set! fft-squelch-amount (/ (.value info) 100)))
+					     (lambda* (w context #:optional info)
+						      (set! fft-squelch-amount (/ (.value (if (provided? 'snd-gtk)
+											      (GTK_ADJUSTMENT w) 
+											      info))
+										  100)))
                                              100))))))
         (activate-dialog fft-squelch-dialog))
 
