84a82242604ba219842dbfad99b168474367a519
schottstaedt <schottstaedt>
2009-12-08 13:54:45 +0000

    bugs

--- sndplay.c
+++ sndplay.c
@@ -81,7 +81,7 @@ static int main_not_alsa(int argc, char *argv[])
     {
       if (strcmp(argv[i], "-describe") == 0) 
 	{
-	  fprintf(stdout, mus_audio_describe()); 
+	  fprintf(stdout, "%s", mus_audio_describe()); 
 	  exit(0);
 	}
       else
