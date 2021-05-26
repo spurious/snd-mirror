15cb4e932125c4a42c4fc75c77f64b6d9b929ac0
schottstaedt <schottstaedt@b278ec2f-9027-4a79-a958-f306da2c2c68>
2021-05-26 09:40:26 +0000

    bugs
    git-svn-id: svn://svn.code.sf.net/p/snd/svn1/trunk@1174 b278ec2f-9027-4a79-a958-f306da2c2c68

--- snd.h
+++ snd.h
@@ -47,11 +47,11 @@
 
 #include "snd-strings.h"
 
-#define SND_DATE "25-May-21"
+#define SND_DATE "27-May-21"
 #ifndef SND_VERSION
-#define SND_VERSION "21.4"
+#define SND_VERSION "21.5"
 #endif
 #define SND_MAJOR_VERSION "21"
-#define SND_MINOR_VERSION "4"
+#define SND_MINOR_VERSION "5"
 
 #endif
